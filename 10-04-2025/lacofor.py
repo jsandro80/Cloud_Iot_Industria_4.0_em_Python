8a3b4933c1cb67668d5256243be9e93d65e7f1cb
UniFanor <UniFanor@FNS-WS-038925.EDUC.ACAD>
2025-04-10 20:33:12 -0300

    lacofor

---
+++ 10-04-2025/lacofor.py
@@ -0,0 +1,8 @@
+#nesse programa, iremos demostrar aplicação básica do laço ou loop for
+#criando laco for
+# for i in range(5):
+#     print(i) #imprime o valor de i em cada iteração do laço for
+lista = ["a", "b", "c", "d", "r","g","h","i"]
+for i in lista:
+    print (i)
+print (lista[7])
